5c6b2cd36ee001981bafcd7d7fb19a440ce2c48a
Prerna <shahiprerna94@gmail.com>
2022-09-09 00:23:55 +1000

    attempting task2

--- week7/index.js
+++ week7/index.js
@@ -1,17 +1,30 @@
+//Task 1///
+//Global Component
 Vue.component('component1', {
     template: '<div>This is component1</div>',
 });
 
+//Local Component
 var component2 = {
     template: '<div>This is component2</div>',
 };
 
+// var component3 = {
+//     template: '<div> {{dog.name}} is playing </div>',
+//     // props: [dog]
+// };
+
+
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!',
+        // message: 'Hello Vue!',
+        // dog: {
+        //     name: 'Cuddles',
+        // },
     },
     components:{
         component2,
+        // component3,
     }
 });
