958d88528fe70d4a647dfb3fccfb114d43b0afe5
Prerna <shahiprerna94@gmail.com>
2022-09-09 15:26:05 +1000

    week6 update

--- A3/index.js
+++ A3/index.js
@@ -2,5 +2,12 @@ var app = new Vue({
     el: '#app',
     data: {
         exist: true,
+        name: '',
+        phoneNum: '',
+        enquiry: '',
     },
 });
+
+function confirm(){
+    document.getElementById('confirmMessage').style.display = 'block';
+}
