2c59237df57725c8a7818624e685e0d8cfa160ce
Prerna <shahiprerna94@gmail.com>
2022-09-19 15:58:24 +1000

    reviewing functionality

--- A3/js/app.js
+++ A3/js/app.js
@@ -3,12 +3,23 @@ Vue.component('contact', {
     template: '<div><p>email: equiries@selfdev.com,  number: 300 453 234</p></div>',
 });
 
-Vue.component('currentBlogs', {
-    template: '<div><p>Blog1</p></div>',
+//Routing//
+var Home = { template: "<div><h1>Home</h1><p>This is home</p></div>" };
+var About = { template: "<div><h1>About</h1><p>This is some information about our awesome company.</p></div>" };
+
+var routes = [
+  { path: '/', component: Home },
+  { path: '/contactUs', component: About}
+];
+
+var router = new VueRouter({
+  routes: routes,
 });
+//Routing//
 
 var app = new Vue({
     el: '#app',
+    router: router, //Routing//
     data: {
         exist: true,
         name: '',
@@ -21,3 +32,5 @@ var app = new Vue({
 function confirm(){
     document.getElementById('confirmMessage').style.display = 'block';
 }
+
+
