7c9bd0c51eede6568f1eba55bcbb4e74edb276a3
Prerna <shahiprerna94@gmail.com>
2022-09-19 18:44:09 +1000

    responsive navbar

--- A3/js/app.js
+++ A3/js/app.js
@@ -33,4 +33,12 @@ function confirm(){
     document.getElementById('confirmMessage').style.display = 'block';
 }
 
+function myFunction() {
+  var x = document.getElementById("mynav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
 
